8d0e7506f99806a7cce5432d6bfe796ebc404b0f
Johannes <tormoehlen.johannes@live.de>
2021-07-16 15:17:04 +0200

    EBFieldSolver

--- src/FieldUtil.py
+++ src/FieldUtil.py
@@ -11,22 +11,60 @@ def render_anim(t, loc):
             writer.append_data(image)
 
 
-def render_frame(x_label, y_label, counter, t, loc):
+def render_frame(x_label, y_label, counter, t, loc, x_limit=[], y_limit=[], aspect=True):
+    if len(x_limit) > 1:
+        plt.gca().set(xlim=(x_limit[0], x_limit[1]), ylim=(y_limit[0], y_limit[1]))
+    if aspect:
+        plt.gca().set_aspect('equal')
+    else:
+        plt.gca().set_aspect('auto')
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    plt.gca().set_aspect('equal')
     plt.savefig('img/static/' + loc + str(counter) + '.png')
     plt.cla()
     print(loc + '#' + str(counter + 1) + "/" + str(len(t)))
 
 
-def plot_arrows(x, y, f_x, f_y, cmap=None, map_z=0.0, normalize=False):
+def plot_arrows(x, y, f_x, f_y, cmap=None, map_z=1.0, normalize=False, cap=0):
     if normalize:
         f_norm = np.hypot(f_x, f_y)
     else:
         f_norm = 1.0
+    if cap > 0:
+        f_x, f_y = trim(f_x, f_y, cap)
     if cmap is None:
+        plt.gca().set_facecolor('white')
         plt.quiver(x, y, f_x / f_norm, f_y / f_norm)
     else:
+        plt.gca().set_facecolor('black')
         plt.rcParams['image.cmap'] = cmap
-        plt.quiver(x, y, f_x / f_norm, f_y / f_norm, map_z)
+        plt.quiver(x, y, f_x / f_norm, f_y / f_norm, np.hypot(f_x, f_y))
+
+
+def trim(f_x, f_y, cap):
+    for i in range(len(f_x)):
+        for j in range(len(f_y)):
+            if f_x[i][j] > cap:
+                f_x[i][j] = cap
+            if f_x[i][j] < -cap:
+                f_x[i][j] = -cap
+            if f_y[i][j] > cap:
+                f_y[i][j] = cap
+            if f_y[i][j] < -cap:
+                f_y[i][j] = -cap
+    return f_x, f_y
+
+
+def plot_normal(x, f_x):
+    plt.gca().set_facecolor('white')
+    plt.plot(x, f_x)
+
+
+def plot_contour(x, y, z):
+    plt.gca().set_facecolor('white')
+    plt.contour(x, y, z, levels=np.linspace(np.min(z), np.max(z), 10))
+
+
+def plot_intensity(x, y, z):
+    plt.gca().set_facecolor('black')
+    plt.pcolormesh(x, y, z, cmap='hot')
--- src/HertzDipole.py
+++ src/HertzDipole.py
@@ -2,17 +2,20 @@ import sys
 
 import numpy as np
 import FieldUtil as util
+import matplotlib.colors as colors
 
 "++++++++++++++++++++++Constants++++++++++++++++++++++"
+_frequency = 500E6
+_power = 1
+_R = 0.1
+
 _c = 299792458.
 _pi = np.pi
 _mu0 = 4 * _pi * 1E-7
 _epsilon0 = 8.85 * 1E-12
-_frequency = 500E6
 _T = 1 / _frequency
 _omega = 2 * np.pi * _frequency
 _wavelength = _c / _frequency
-_power = 1
 _p_norm = np.sqrt(12 * _pi * _c * _power / (_mu0 * _omega ** 4))
 _p_0 = np.array([0.0, 0.0, _p_norm])
 
@@ -20,6 +23,7 @@ _p_0 = np.array([0.0, 0.0, _p_norm])
 def dipole_E(x, y, z, p, t):
     r = np.array([x, y, z])
     r_norm = np.linalg.norm(r)
+    r = r / r_norm
     r_cross_p = np.cross(r, p)
     rcrossp_cross_r = np.cross(r_cross_p, r)
     r_dot_p = np.dot(r, p)
@@ -33,12 +37,14 @@ def dipole_E(x, y, z, p, t):
     c6 = 1j * (c2 - (_omega * t))
 
     E = c1 * ((rcrossp_cross_r * c3) + (r_dot_rdotp * (c4 - c5))) * np.exp(c6)
+
     return E
 
 
 def dipole_H(x, y, z, p, t):
     r = np.array([x, y, z])
     r_norm = np.linalg.norm(r)
+    r = r / r_norm
     r_cross_p = np.cross(r, p)
 
     c1 = (_omega ** 3 / (4 * np.pi * _c ** 2))
@@ -72,7 +78,7 @@ if __name__ == "__main__":
     X1, Z = np.meshgrid(x, z)
     X2, Y = np.meshgrid(x, y)
 
-    nt = 20
+    nt = 25
     t0 = 0
     t1 = _T
     t = np.linspace(t0, t1, nt)
@@ -81,14 +87,29 @@ if __name__ == "__main__":
     Hx, Hy = np.zeros((len(x), len(y))), np.zeros((len(x), len(y)))
     Sx, Sz = np.zeros((len(x), len(z))), np.zeros((len(x), len(z)))
 
+    x_0 = np.linspace(1E-3, 8 * _wavelength, 100)
+
+    E_xz_0, H_xy_0 = np.zeros(len(x_0)), np.zeros((len(x_0)))
+
     counter = 0
     for dt in t:
-        p = _p_0 * np.exp(-1j * _omega * dt)
+        p = _p_0 * np.exp(-1 * 1j * _omega * dt)
         for i in range(len(X1)):
             for j in range(len(Z)):
+
+                # if np.hypot(X1[i][j], Z[i][j]) <= _R:
+                #     E = np.array([0.0, 0.0, 0.0])
+                #     H = np.array([0.0, 0.0, 0.0])
+                #     S = np.array([0.0, 0.0, 0.0])
+                # else:
+                #     E = dipole_E(X1[i][j], 0, Z[i][j], p, dt)
+                #     H = dipole_H(X2[i][j], Y[i][j], 0, p, dt)
+                #     S = dipole_Poynting(E, H)
+
                 E = dipole_E(X1[i][j], 0, Z[i][j], p, dt)
                 H = dipole_H(X2[i][j], Y[i][j], 0, p, dt)
                 S = dipole_Poynting(E, H)
+
                 Ex[i][j] = np.real(E[0])
                 Ez[i][j] = np.real(E[2])
                 Hx[i][j] = np.real(H[0])
@@ -96,24 +117,38 @@ if __name__ == "__main__":
                 Sx[i][j] = np.real(S[0])
                 Sz[i][j] = np.real(S[2])
 
+        for k in range(len(x_0)):
+            E = dipole_E(x_0[k], 0, 0, p, dt)
+            H = dipole_H(x_0[k], 0, 0, p, dt)
+            E_xz_0[k] = np.real(dipole_E(x_0[k], 0, 0, p, dt)[2])
+            H_xy_0[k] = np.real(dipole_H(x_0[k], 0, 0, p, dt)[1])
+
         # S_norm = np.hypot(Sx, Sz)
         # plt.quiver(X1, Z, Sx / S_norm, Sz / S_norm)
         # plt.contour(X1, Z, E_norm, levels=np.linspace(2, 10, 4))
         # plt.pcolormesh(X1, Z, E_norm, cmap='hot')
 
-        util.plot_arrows(X1, Z, Ex, Ez, 'winter', Ez / np.hypot(Ex, Ez), normalize=True)
+        util.plot_arrows(X1, Z, Ex, Ez, cmap='winter', cap=5.0)
+        # util.plot_intensity(X1, Z, np.hypot(Ex, Ez))
+        # util.plot_contour(X1, Z, np.hypot(Ex, Ez))
         util.render_frame(r'$x/\lambda$', r'$z/\lambda$', counter, t, 'dipole_E')
 
-        util.plot_arrows(X2 / _wavelength, Y / _wavelength, Hx, Hy, 'cool', 1 / (2.0 + np.hypot(X2, Y)))
+        util.plot_arrows(X2, Y, Hx, Hy, cmap='cool', cap=0.05)
         util.render_frame(r'$x/\lambda$', r'$y/\lambda$', counter, t, 'dipole_H')
 
-        util.plot_arrows(X1, Z, Sx / 10, Sz / 10)
+        # S_dz, S_dx = np.gradient(np.hypot(Sx, Sz))
+        util.plot_arrows(X1, Z, Sx, Sz, cmap='hot', cap=0.1)
         util.render_frame(r'$x/\lambda$', r'$z/\lambda$', counter, t, 'dipole_S')
+
+        util.plot_normal(x_0, -E_xz_0)
+        util.plot_normal(x_0, 1000*H_xy_0)
+        util.render_frame(r'$x/$m', r'$E_{z=0}$ and $H_{y=0}$', counter, t, 'E_xz_0_H_xy_0', [0, 6], [-100, 100], aspect=False)
 
         counter = counter + 1
 
     util.render_anim(t, 'dipole_H')
     util.render_anim(t, 'dipole_E')
     util.render_anim(t, 'dipole_S')
+    util.render_anim(t, 'E_xz_0_H_xy_0')
 
     sys.exit(0)
